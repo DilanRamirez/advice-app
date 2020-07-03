6a13c3bcde0d33e60260ac5314d18ed536d4cffd
Dilan R. Ramirez <47306089+DilanRamirez@users.noreply.github.com>
2020-07-03 11:51:34 -0700

    Updates

---
+++ precache-manifest.7b75a8574a3a138a060ed00bad63c429.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d4451307fc5333ec4d7b7a67261387",
+    "url": "/advice-app/index.html"
+  },
+  {
+    "revision": "998cb5b9dc7ba14db585",
+    "url": "/advice-app/static/css/main.aef59ae1.chunk.css"
+  },
+  {
+    "revision": "eb0b7ec367d249ffdbad",
+    "url": "/advice-app/static/js/2.33b64ec1.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/advice-app/static/js/2.33b64ec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "998cb5b9dc7ba14db585",
+    "url": "/advice-app/static/js/main.eadabb7e.chunk.js"
+  },
+  {
+    "revision": "a2a04141c9841a4cf9c4",
+    "url": "/advice-app/static/js/runtime-main.0502de4b.js"
+  }
+]);
